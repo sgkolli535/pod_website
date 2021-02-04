fd84628256bcac095d3b52813339b6d3969b6823
gladishd <gladish.dean@gmail.com>
2021-02-04 17:39:27 -0600

    fixing merge conflict

--- src/meetThePod.js
+++ src/meetThePod.js
@@ -2,13 +2,8 @@ import './App.css';
 
 function MeetThePod() {
   return (
-<<<<<<< HEAD
     <div id="meet">
       <h1>Meet the Pod</h1>
-=======
-    <div>
-      meet the pod!
->>>>>>> f5a2e6e1c41e9bc438c9b411ec71984f61135ce5
     </div>
   );
 }
--- src/projects.js
+++ src/projects.js
@@ -2,13 +2,8 @@ import './App.css';
 
 function Projects() {
   return (
-<<<<<<< HEAD
     <div id="projects">
       <h1>Projects</h1>
-=======
-    <div>
-      projects
->>>>>>> f5a2e6e1c41e9bc438c9b411ec71984f61135ce5
     </div>
   );
 }
