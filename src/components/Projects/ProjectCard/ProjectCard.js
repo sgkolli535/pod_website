90a55629f9a1563f9c6dec95ad2f0cc31aa5a38c
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-02-07 20:49:56 -0600

    update projects component

--- src/components/Projects/ProjectCard/ProjectCard.js
+++ src/components/Projects/ProjectCard/ProjectCard.js
@@ -1,12 +1,22 @@
 import styles from './ProjectCard.module.css';
-import {Row, Col, Badge} from 'react-bootstrap';
+import {Row, Col, Badge, Button} from 'react-bootstrap';
 
 function ProjectCard(props){
   const badgeList = ["primary", "secondary", "success", "danger", "warning", "light"];
+
+  const assignBadge = props.tech.map((ele,i)=>{
+    let selectStyle = badgeList[Math.floor(Math.random() * badgeList.length)];
+    return <Badge key= {i} variant={selectStyle} className={styles.tech_stk}>{ele}</Badge>;
+  });
+
+  const links = Object.keys(props.links).map((link,i) => {
+    return <Button key={i} href={props.links[link]} variant="info" className={styles.tech_stk}>{link}</Button>;
+  })
+
   return (
     <Row className={styles.r}>
       <Col className={styles.c}>
-        <img className={styles.pic} src="/_podImg/dummy.png"/>
+        <img className={styles.pic} src={props.image}/>
       </Col>
       <Col className={styles.c}>
         <div>
@@ -16,9 +26,15 @@ function ProjectCard(props){
             <p>{props.description}</p>
           </div>
           <div>
-            <span className={styles.des}>Technologies used</span>
+            <span className={styles.des}>Tech Stack</span>
             <p>
-              <Badge variant="primary">Primary</Badge>{' '}
+              {assignBadge}
+            </p>
+          </div>
+          <div>
+            <span className={styles.des}>Links</span>
+            <p>
+              {links}
             </p>
           </div>
         </div>
